7cfc353eea02add33596179ce141889631b1e9c5
abdulahad0110 <abdulahad200325@gmail.com>
2024-03-13 15:55:00 +0500

    JS 04

---
+++ script.js
@@ -0,0 +1,23 @@
+// Question 1
+var num1 = 23, num2 = 21, num3 = 58;
+
+// Question 2
+
+// Legal variable name
+var myName = "Abdul Ahad";
+var age = 20;
+var rollNo = 214770;
+var collegeName = "ABC College";
+var cityName = "Karachi";
+// illegal variable name
+var myname = "abdul ahad";
+var rollno = 21;
+var collegename = "karachi";
+var countrynamecode = 'pk';
+var citynameaddress = "abc";
+//  Question 3
+
+document.write("<h1>Rules for naming JS variables</h1>")
+document.write("<br/>Variable names can only contain " + myName + ", " + age + ", " + collegeName + "and " + cityName)
+document.write("<br/>Variable names are case " + cityName)
+document.write("<br/>Variable names should not be JS " + countrynamecode)
